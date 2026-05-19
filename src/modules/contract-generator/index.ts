9665d5fcab1bcb8e77470fdaee5defafbe74f7c4
Lukyanenko A. <alla.alla.luk@ya.ru>
2026-05-19 19:49:48 +0300

    feat: добавлена поддержка ERC-721 в генератор контрактов

--- src/modules/contract-generator/index.ts
+++ src/modules/contract-generator/index.ts
@@ -2,3 +2,5 @@
 export class ContractGenerator { 
   generateERC20() { return "ERC20 code"; } 
 } 
+// Contract Generator module - ERC721 support added 
+  generateERC721() { return "ERC721 code"; } 
